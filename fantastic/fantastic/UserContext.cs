9a561b93d9631dbd1cc232178012ea6363ac911a
Thato <t.chitja369@gmail.com>
2023-09-20 09:59:52 +0200

    Add UserContext class

---
+++ fantastic/fantastic/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using fantastic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fantastic
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Organisation> Organisations { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Project>().ToTable("Project");
+            modelBuilder.Entity<Organisation>().ToTable("Organisation");
+        }
+    }
+}
