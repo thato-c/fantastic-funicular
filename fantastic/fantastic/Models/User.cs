c742c9863ee0797059810b3792b5399f979076cb
Thato <t.chitja369@gmail.com>
2023-09-20 09:13:22 +0200

    Add User Model

---
+++ fantastic/fantastic/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace fantastic.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserEmail { get; set; } 
+        public string Password { get; set; }
+    }
+}
