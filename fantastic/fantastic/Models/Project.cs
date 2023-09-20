58e0a2618c3b3c99f05d550924bf0a80b92b3651
Thato <t.chitja369@gmail.com>
2023-09-20 09:33:52 +0200

    Add Project Model

---
+++ fantastic/fantastic/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace fantastic.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Project_Name { get; set; }
+        public string Project_Description { get; set;}
+        public int UserId { get; set; }
+        public int OrganisationId { get; set; }
+    }
+}
